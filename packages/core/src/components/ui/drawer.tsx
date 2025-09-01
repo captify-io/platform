02619e8bfd48971bb9f2e5dd9a28abebcbb07c7b
Mike Johnson <52497963+m1kej0hns0n@users.noreply.github.com>
2025-09-01 14:01:55 -0500

    drawer.trigger

--- packages/core/src/components/ui/drawer.tsx
+++ packages/core/src/components/ui/drawer.tsx
@@ -16,11 +16,11 @@ const Drawer = ({
 )
 Drawer.displayName = "Drawer"
 
-const DrawerTrigger = DrawerPrimitive.Trigger
+const DrawerTrigger: typeof DrawerPrimitive.Trigger = DrawerPrimitive.Trigger
 
 const DrawerPortal = DrawerPrimitive.Portal
 
-const DrawerClose = DrawerPrimitive.Close
+const DrawerClose: typeof DrawerPrimitive.Close = DrawerPrimitive.Close
 
 const DrawerOverlay = React.forwardRef<
   React.ElementRef<typeof DrawerPrimitive.Overlay>,
