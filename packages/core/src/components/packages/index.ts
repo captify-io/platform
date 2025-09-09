b533a4af1b780b793c9cfe02bb52647ca7a232c2
Mike Johnson <52497963+m1kej0hns0n@users.noreply.github.com>
2025-09-09 10:25:54 -0500

    Delete packages directory
    old code

--- packages/core/src/components/packages/index.ts
+++
@@ -1,6 +0,0 @@
-// Package management components
-export { PackageAgentPanel } from "./PackageAgentPanel";
-export { PackageContentPanel } from "./PackageContentPanel";
-export { PackagePageRouter } from "./PackagePageRouter";
-export { getPackageLoader, loadPackageRegistry } from "./PackageRegistry";
-export { PackageRenderer } from "./PackageRenderer";
